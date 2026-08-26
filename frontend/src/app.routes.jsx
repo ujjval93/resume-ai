0a9ef4b8f51b35832ec071c90757e1b66d496967
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-08-26 21:45:35 +0530

    install new packages and change css file

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,4 +1,26 @@
 import { createBrowserRouter } from "react-router";
+import Login from "./features/auth/pages/Login.jsx";
+import Register from "./features/auth/pages/Register.jsx";
+//import Protected from "./features/auth/components/Protected";
+//import Home from "./features/interview/pages/Home";
+//import Interview from "./features/interview/pages/Interview";
 
 
-export const router = createBrowserRouter([])
+export const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    // {
+    //     path: "/",
+    //     element: <Protected><Home /></Protected>
+    // },
+    // {
+    //     path:"/interview/:interviewId",
+    //     element: <Protected><Interview /></Protected>
+    // }
+])
