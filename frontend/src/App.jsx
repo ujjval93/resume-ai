dba71a24ea25b9c24dd90c2cbf732590f8193dec
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-08-27 12:12:34 +0530

    complete login and register user interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
 
-const App = () => {
+function App() {
+
   return (
-    <div>App</div>
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
+    </AuthProvider>
   )
 }
 
