d2971d85e22cdcb382628000d808be13b4ed0aa3
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-08-30 15:34:58 +0530

    implement ai and add all backend features

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require('./src/app');
 require('dotenv').config();
 const connectDB = require('./src/config/database');
 
-connectDB();
+connectDB();  
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const cookieParser = require('cookie-parser')
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
-/* Require all the routes here */
-const authRouter = require('./routes/auth.routes');
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+//const interviewRouter = require("./routes/interview.routes")
 
-//using all the routes here
-app.use('/api/auth', authRouter);
 
-module.exports = app;
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+//app.use("/api/interview", interviewRouter)
+
+
+
+module.exports = app
